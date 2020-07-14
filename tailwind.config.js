7c730e85f5e8f4eb47db670f0065923fca794eb0
Aaron Fabricio <aaronfabricio00@gmail.com>
2020-07-14 13:32:14 -0400

    se desarrollo biografia

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors:{
+        "primary":"#0e1d40",
+        "secondary":"#15bcc0"
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
